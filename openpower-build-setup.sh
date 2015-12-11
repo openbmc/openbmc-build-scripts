c4c6d43819cf68b9c8b18ef9e40dc2cbdd29b9e8
Joel Stanley <joel@jms.id.au>
2015-12-11 17:27:15 +1030

    openpower-build-setup: Add vim-common for xxd
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- openpower-build-setup.sh
+++ openpower-build-setup.sh
@@ -46,7 +46,8 @@ RUN echo $(date +%s) && apt-get update && \
 	apt-get install -y \
 	cscope ctags libz-dev libexpat-dev python language-pack-en texinfo \
 	build-essential g++ git bison flex unzip libxml-simple-perl \
-	libxml-sax-perl libxml2-dev libxml2-utils xsltproc wget cpio bc
+	libxml-sax-perl libxml2-dev libxml2-utils xsltproc wget cpio bc \
+	vim-common
 RUN groupadd -g ${GROUPS} ${USER} && useradd -d ${HOME} -m -u ${UID} -g ${GROUPS} ${USER}
 
 USER ${USER}
