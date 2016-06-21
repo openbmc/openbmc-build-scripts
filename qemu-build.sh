0b8511820f59d09596d56ae7bde577f71c40b96a
Joel Stanley <joel@jms.id.au>
2016-06-21 23:51:19 +0930

    Add script for building qemu
    This builds a command line qemu binary under Ubuntu 16.04.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>

---
+++ qemu-build.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# This build script is for running the Jenkins builds using docker.
+
+# Trace bash processing
+set -x
+
+# Default variables
+WORKSPACE=${WORKSPACE:-${HOME}/${RANDOM}${RANDOM}}
+http_proxy=${http_proxy:-}
+
+# Timestamp for job
+echo "Build started, $(date)"
+
+# Configure docker build
+if [[ -n "${http_proxy}" ]]; then
+PROXY="RUN echo \"Acquire::http::Proxy \\"\"${http_proxy}/\\"\";\" > /etc/apt/apt.conf.d/000apt-cacher-ng-proxy"
+fi
+
+Dockerfile=$(cat << EOF
+FROM ubuntu:16.04
+
+${PROXY}
+
+RUN echo "deb http://au.archive.ubuntu.com/ubuntu/ xenial main" > \
+    /etc/apt/sources.list
+
+
+ENV DEBIAN_FRONTEND noninteractive
+RUN apt-get update && apt-get install -yy --no-install-recommends \
+	flex bison \
+	libpixman-1-dev \
+	libglib2.0-dev \
+	libfdt-dev \
+	make python-yaml gcc libc6-dev
+
+
+RUN grep -q ${GROUPS} /etc/group || groupadd -g ${GROUPS} ${USER}
+RUN grep -q ${UID} /etc/passwd || useradd -d ${HOME} -m -u ${UID} -g ${GROUPS} ${USER}
+
+USER ${USER}
+ENV HOME ${HOME}
+RUN /bin/bash
+EOF
+)
+
+# Build the docker container
+docker build -t qemu-build/ubuntu - <<< "${Dockerfile}"
+if [[ "$?" -ne 0 ]]; then
+  echo "Failed to build docker container."
+  exit 1
+fi
+
+# Create the docker run script
+export PROXY_HOST=${http_proxy/#http*:\/\/}
+export PROXY_HOST=${PROXY_HOST/%:[0-9]*}
+export PROXY_PORT=${http_proxy/#http*:\/\/*:}
+
+mkdir -p ${WORKSPACE}
+
+cat > "${WORKSPACE}"/build.sh << EOF_SCRIPT
+#!/bin/bash
+
+set -x
+
+cd ${WORKSPACE}
+
+gcc --version
+
+# Go into the source directory (the script will put us in a build subdir)
+cd qemu
+# disable anything that requires us to pull in X
+./configure --target-list=arm-softmmu \
+	--disable-spice --disable-docs --disable-gtk --disable-smartcard \
+	--disable-usb-redir --disable-libusb --disable-sdl --disable-gnutls \
+	--disable-vte --disable-vnc --disable-vnc-png
+make -j4
+
+EOF_SCRIPT
+
+chmod a+x ${WORKSPACE}/build.sh
+
+# Run the docker container, execute the build script we just built
+docker run --rm=true -e WORKSPACE=${WORKSPACE} --user="${USER}" \
+  -w "${HOME}" -v "${HOME}":"${HOME}" -t qemu-build/ubuntu ${WORKSPACE}/build.sh
