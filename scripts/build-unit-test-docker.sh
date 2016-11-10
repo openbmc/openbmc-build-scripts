076521a691cd5210bdac336709c2e55d5d8f4eb6
Matthew Barth <msbarth@us.ibm.com>
2016-11-10 14:59:52 -0600

    Create base build unit test docker script
    Base script to configure and build a docker image for unit tests
    
    Change-Id: Iea15d8c57760376c2f905d7f889d9ea483f6b0ad
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ scripts/build-unit-test-docker.sh
@@ -0,0 +1,66 @@
+#!/bin/bash -xe
+#
+# Build the required docker image to run package unit tests
+#
+#  Parameters:
+#   parm1:  <optional, the name of the docker image to generate>
+#            default is openbmc/ubuntu-unit-test
+
+set -uo pipefail
+
+DOCKER_IMG_NAME=${1:-"openbmc/ubuntu-unit-test"}
+
+# Determine our architecture, ppc64le or the other one
+if [ $(uname -m) == "ppc64le" ]; then
+    DOCKER_BASE="ppc64le/"
+    ZESTY_MIRROR="deb http://ports.ubuntu.com/ubuntu-ports zesty main universe"
+else
+    DOCKER_BASE=""
+    ZESTY_MIRROR="deb http://mirrors.kernel.org/ubuntu zesty main universe"
+fi
+
+################################# docker img # #################################
+# Create docker image that can run package unit tests
+Dockerfile=$(cat << EOF
+FROM ${DOCKER_BASE}ubuntu:latest
+
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN apt-get update && apt-get install -yy \
+    gcc \
+    g++ \
+    libc6-dev \
+    libtool \
+    cmake \
+    python \
+    python-dev \
+    python-setuptools \
+    autoconf \
+    automake \
+    pkg-config \
+    libsystemd-dev \
+    sudo \
+    wget \
+    git \
+    vim
+
+RUN echo ${ZESTY_MIRROR} >> /etc/apt/sources.list
+RUN apt-get update && apt-get install -yy \
+    autoconf-archive=20160916-1 \
+    libgtest-dev=1.7.0-4ubuntu1
+
+RUN cd /usr/src/gtest && sudo cmake . && sudo make && sudo mv libg* /usr/lib/
+
+RUN grep -q ${GROUPS} /etc/group || groupadd -g ${GROUPS} ${USER}
+RUN grep -q ${UID} /etc/passwd || useradd -d ${HOME} -m -u ${UID} -g ${GROUPS} \
+                    ${USER}
+USER ${USER}
+ENV HOME ${HOME}
+RUN /bin/bash
+EOF
+)
+
+################################# docker img # #################################
+
+# Build above image
+docker build -t ${DOCKER_IMG_NAME} - <<< "${Dockerfile}"
