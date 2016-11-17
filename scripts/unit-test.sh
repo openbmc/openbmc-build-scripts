f4a9b0b7103b1f0d70460381444afa4fedf11813
Matthew Barth <msbarth@us.ibm.com>
2016-11-17 14:29:43 -0600

    WIP: Script to run within unit test docker image
    Script to run within docker image to determine & install a package's
    dependencies for running the package's unit tests.
    
    Change-Id: I9395685bc62742463d177b4e04c31f1dddf6bbe6
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ scripts/unit-test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -xe
+#
+# Execute the given repository's unit tests
+#
+set -o pipefail
+
+WORKSPACE=${WORKSPACE:-${HOME}}
+
+# Go into repository to be tested
+cd ${WORKSPACE}
+
+# TEMP? Make sure in a valid repository
+if [ -e "bootstrap.sh" ]; then
+# Configure package
+./bootstrap.sh
+./configure ${CONFIGURE_FLAGS}
+
+# Build package
+make
+
+# Install package
+make install
+
+# Run package unit tests
+make check
+fi
+
+# Allow workspace cleanup
+chmod ugo+rw
