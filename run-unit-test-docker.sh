c5dec75eb39d12333e9286d22e0b28d9ef58fb3e
Matthew Barth <msbarth@us.ibm.com>
2016-11-18 13:14:37 -0600

    Create run unit test docker script
    Wrapper script for Jenkins that does everything necessary to build,
    deploy, and run a docker image for unit tests.
    
    Change-Id: I1436b84d078659c928804bb41aa0772176f7f64e
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ run-unit-test-docker.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -xe
+
+# This build script is for running the Jenkins unit test builds using docker.
+#
+# It uses a few variables which are part of Jenkins build job matrix:
+#   distro = fedora|ubuntu|ubuntu:14.04|ubuntu:16.04
+#   WORKSPACE = <location of unit test execution script>
+
+# Trace bash processing. Set -e so when a step fails, we fail the build
+set -uo pipefail
+
+# Default variables
+DOCKER_IMG_NAME="openbmc/ubuntu-unit-test"
+DISTRO=${distro:-ubuntu:latest}
+WORKSPACE=${WORKSPACE:-${TMP}/unit-test${RANDOM}}
+OBMC_BUILD_SCRIPTS="openbmc-build-scripts"
+UNIT_TEST_PY_DIR="scripts"
+UNIT_TEST_PY="unit-test.py"
+
+# Timestamp for job
+echo "Unit test build started, $(date)"
+
+# Currently only support ubuntu:latest due to systemd requirements
+if [[ "${DISTRO}" == "ubuntu"* ]]; then
+    DISTRO="ubuntu:latest"
+elif [[ "${DISTRO}" == "fedora" ]]; then
+    echo "Distro (${DISTRO}) not supported, running as ubuntu"
+    DISTRO="ubuntu:latest"
+fi
+
+# Check workspace, build scripts, and package to be unit tested exists
+if [ ! -d "${WORKSPACE}" ]; then
+    echo "Workspace(${WORKSPACE}) doesn't exist, exiting..."
+    exit 1
+fi
+if [ ! -d "${WORKSPACE}/${OBMC_BUILD_SCRIPTS}" ]; then
+    echo "Package(${OBMC_BUILD_SCRIPTS}) not found in ${WORKSPACE}, exiting..."
+    exit 1
+fi
+if [ ! -d "${WORKSPACE}/${UNIT_TEST_PKG}" ]; then
+    echo "Package(${UNIT_TEST_PKG}) not found in ${WORKSPACE}, exiting..."
+    exit 1
+fi
+
+# Copy unit test script into workspace
+cp ${WORKSPACE}/${OBMC_BUILD_SCRIPTS}/${UNIT_TEST_PY_DIR}/${UNIT_TEST_PY} \
+${WORKSPACE}/${UNIT_TEST_PY}
+chmod a+x ${WORKSPACE}/${UNIT_TEST_PY}
+
+# Configure docker build
+cd ${WORKSPACE}/${OBMC_BUILD_SCRIPTS}
+echo "Building docker image with build-unit-test-docker.sh"
+./build-unit-test-docker.sh ${DOCKER_IMG_NAME} ${DISTRO}
+
+# Run the docker unit test container with the unit test execution script
+echo "Executing docker image"
+docker run --cap-add=sys_admin --rm=true \
+    -e WORKSPACE=${WORKSPACE}/ \
+    -e UNIT_TEST_PKG=${UNIT_TEST_PKG} \
+    -e EXIT_USER=${USER} \
+    -w "${WORKSPACE}" -v "${WORKSPACE}":"${WORKSPACE}" \
+    -t ${DOCKER_IMG_NAME} ${WORKSPACE}/${UNIT_TEST_PY}
+
+# Timestamp for build
+echo "Unit test build completed, $(date)"
