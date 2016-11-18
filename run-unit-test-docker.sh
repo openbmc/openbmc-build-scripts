ac480e2dd54f65e89be91e15b0b22c9f5317c27a
Matthew Barth <msbarth@us.ibm.com>
2016-11-18 13:14:37 -0600

    WIP: Create run unit test docker script
    Wrapper script for Jenkins that does everything necessary to build,
    deploy, and run a docker image for unit tests
    
    Change-Id: I1436b84d078659c928804bb41aa0772176f7f64e
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

---
+++ run-unit-test-docker.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -xe
+
+# This build script is for running the Jenkins unit test builds using docker.
+#
+# It uses a few variables which are part of Jenkins build job matrix:
+#   distro = fedora|ubuntu|ubuntu:14.04|ubuntu:16.04
+#   WORKSPACE = <location of unit test execution script>
+
+# Trace bash processing. Set -e so when a step fails, we fail the build
+set -uo pipefail
+
+# Default variables
+DISTRO=${distro:-ubuntu}
+WORKSPACE=${WORKSPACE:-${HOME}/${RANDOM}${RANDOM}}
+UNIT_TEST_SH="https://raw.githubusercontent.com/openbmc/"
+UNIT_TEST_SH+="openbmc-build-scripts/scripts/unit-tests.sh"
+
+# Timestamp for job
+echo "Unit test build started, $(date)"
+
+# Currently only support ubuntu:latest due to systemd requirements
+if [[ "${DISTRO}" == "ubuntu"* ]]; then
+    DISTRO="ubuntu:latest"
+elif [[ "${DISTRO}" == "fedora" ]]; then
+    echo "Distro (${DISTRO}) not supported, running as ubuntu"
+    DISTRO="ubuntu:latest"
+fi
+
+# Check workspace exists, create if not
+if [ ! -d "${WORKSPACE}" ]; then
+    echo "${WORKSPACE} doesn't exist, creating..."
+    mkdir "${WORKSPACE}"
+fi
+
+# Pull down unit test execution script into workspace
+echo "Retrieving unit test execution script to ${WORKSPACE}"
+#wget ${UNIT_TEST_SH} ${WORKSPACE}/${UNIT_TEST_SH}
+wget https://gerrit.openbmc-project.xyz/cat/1214%2C2%2Cscripts/unit-test.sh%5E0 -O ${WORKSPACE}/unit_test.sh.zip
+echo "Unzipping ${WORKSPACE}/unit_test.sh.zip"
+UNIT_TEST_SH="unit_test.sh"
+unzip -p ${WORKSPACE}/unit_test.sh.zip > ${WORKSPACE}/${UNIT_TEST_SH}
+
+chmod a+x ${WORKSPACE}/${UNIT_TEST_SH}
+
+# Configure docker build
+echo "Building docker image with build-unit-test-docker.sh"
+./build-unit-test-docker.sh openbmc/${DISTRO} ${DISTRO}
+
+# Run the docker unit test container with the unit test execution script
+echo "Executing docker image"
+docker run --cap-add=sys_admin --rm=true -e WORKSPACE=${WORKSPACE} \
+    --user="${USER}" -w "${HOME}" -v "${HOME}":"${HOME}" \
+    -t openbmc/${DISTRO} ${WORKSPACE}/${UNIT_TEST_SH}
+
+# Timestamp for build
+echo "Unit test build completed, $(date)"
+
