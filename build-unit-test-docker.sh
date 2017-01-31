a2366d9ad098fbad8f0a9537de5be35b55ff66d2
Matthew Barth <msbarth@us.ibm.com>
2017-01-31 09:46:20 -0600

    Install pip and add python inflection dependency
    Change-Id: I5fd2fbcc0c4c15964e1337919177f978f70e3db7
    Signed-off-by: Matthew Barth <msbarth@us.ibm.com>

--- build-unit-test-docker.sh
+++ build-unit-test-docker.sh
@@ -59,6 +59,9 @@ RUN apt-get update && apt-get install -yy \
     wget \
     git
 
+RUN easy_install pip
+RUN pip install inflection
+
 RUN wget http://ftpmirror.gnu.org/autoconf-archive/autoconf-archive-2016.09.16.tar.xz
 RUN tar -xJf autoconf-archive-2016.09.16.tar.xz
 RUN cd autoconf-archive-2016.09.16 && ./configure --prefix=/usr && make && make install
