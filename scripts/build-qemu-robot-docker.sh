94089a992d5b03a0d60a089b8e2449ca67ff790e
Rahul Maheshwari <rahulmaheshwari@in.ibm.com>
2017-03-13 06:56:12 -0500

    Install latest ipmitool to run network IPMI in robot.
    Resolves openbmc/openbmc-build-scripts#14
    
    Change-Id: Ib9f38148cd7997261e78df8c04da067381e62b45
    Signed-off-by: Rahul Maheshwari <rahulmaheshwari@in.ibm.com>

--- scripts/build-qemu-robot-docker.sh
+++ scripts/build-qemu-robot-docker.sh
@@ -49,6 +49,8 @@ RUN apt-get update && apt-get install -yy \
     expect \
     curl
 
+RUN apt-get -y install build-essential
+
 RUN easy_install \
     tox \
     pip \
@@ -59,6 +61,13 @@ RUN pip install \
     robotframework-requests \
     robotframework-sshlibrary \
     robotframework-scplibrary
+
+RUN wget https://sourceforge.net/projects/ipmitool/files/ipmitool/1.8.18/ipmitool-1.8.18.tar.bz2
+RUN tar xvfj ipmitool-*.tar.bz2
+RUN ./ipmitool-1.8.18/configure
+RUN make
+RUN cp /src/ipmitool /usr/bin/
+RUN ipmitool -V
 
 RUN grep -q ${GROUPS} /etc/group || groupadd -g ${GROUPS} ${USER}
 RUN grep -q ${UID} /etc/passwd || useradd -d ${HOME} -m -u ${UID} -g ${GROUPS} \
