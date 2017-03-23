2ab9a214e5bb6674afcbd9dc5da8952a7423fabc
Joel Stanley <joel@jms.id.au>
2017-03-23 10:56:27 +1030

    openpower-build-setup: Add openssl dependency
    The kernel is now build with module signing which requires openssl.
    
    Change-Id: I7edd15a699a16acdde5baf5620af0ea4e6e0474b
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- openpower-build-setup.sh
+++ openpower-build-setup.sh
@@ -61,6 +61,7 @@ RUN dnf --refresh repolist && dnf install -y \
 	git \
 	libxml2-devel \
 	ncurses-devel \
+	openssl-devel \
 	patch \
 	perl \
 	perl-bignum \
@@ -107,6 +108,7 @@ RUN apt-get update && apt-get install -yy \
 	flex \
 	g++ \
 	git \
+	libssl-dev \
 	libexpat-dev \
 	libz-dev \
 	libxml-sax-perl \
