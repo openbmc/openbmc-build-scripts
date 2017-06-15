1359613563ee2b716ca688c5b7634358f4c53b63
Alanny Lopez <alanny.lopez@ibm.com>
2017-06-15 11:26:18 -0500

    Git added to Dockerfile
    Added Git, since it seems to no longer be in the base image for Ubuntu.
    Spacing alignment, alphabetized, 1 item per line to apt-get RUN command.
    
    Change-Id: Id30f8196c056d7d71eea5281dc68bf708e359efd
    Signed-off-by: Alanny Lopez <alanny.lopez@ibm.com>

--- qemu-build.sh
+++ qemu-build.sh
@@ -24,11 +24,17 @@ ${PROXY}
 
 ENV DEBIAN_FRONTEND noninteractive
 RUN apt-get update && apt-get install -yy --no-install-recommends \
-	flex bison \
-	libpixman-1-dev \
-	libglib2.0-dev \
-	libfdt-dev \
-	make python-yaml python3-yaml gcc libc6-dev
+    bison \
+    flex \
+    gcc \
+    git \
+    libc6-dev \
+    libfdt-dev \
+    libglib2.0-dev \
+    libpixman-1-dev \
+    make \
+    python-yaml \
+    python3-yaml
 
 RUN grep -q ${GROUPS} /etc/group || groupadd -g ${GROUPS} ${USER}
 RUN grep -q ${UID} /etc/passwd || useradd -d ${HOME} -m -u ${UID} -g ${GROUPS} ${USER}
