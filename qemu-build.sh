644ff116b8fca4766a265e98a704cb2be8ad8d13
Alanny Lopez <alanny.lopez@ibm.com>
2017-06-27 14:20:48 -0500

    Isolated the build directory
    The build directory was whatever path the qemudir had. To adjust for allowing
    external mounts to be used such as NFS have made it so that the script will
    use the qemudir to create an internal copy where the build will happen so that
    files in the qemudir will not be affected. The artifacts for the image will
    be found in ${WORKSPACE}/arm-softmmu which will be copied out of the build.
    
    Change-Id: I21cfc8c5a7817de0b8641bb4d4f0b635904dd1bc
    Signed-off-by: Alanny Lopez <alanny.lopez@ibm.com>

--- qemu-build.sh
+++ qemu-build.sh
@@ -77,8 +77,11 @@ cat > "${WORKSPACE}"/build.sh << EOF_SCRIPT
 
 set -x
 
+# create a copy of the qemudir in /qemu to use as the build directory
+cp -r ${qemudir}/* /qemu
+
 # Go into the source directory (the script will put us in a build subdir)
-cd ${qemudir}
+cd /qemu
 
 gcc --version
 git submodule update --init dtc
@@ -98,6 +101,7 @@ git submodule update --init dtc
     --disable-vnc-png
 make -j4
 
+cp -r /qemu/arm-softmmu ${WORKSPACE}/arm-softmmu
 EOF_SCRIPT
 
 chmod a+x ${WORKSPACE}/build.sh
@@ -124,6 +128,8 @@ RUN apt-get update && apt-get install -yy --no-install-recommends \
 
 RUN grep -q ${GROUPS} /etc/group || groupadd -g ${GROUPS} ${USER}
 RUN grep -q ${UID} /etc/passwd || useradd -d ${HOME} -m -u ${UID} -g ${GROUPS} ${USER}
+RUN mkdir /qemu \
+    chown -r ${UID}:${UID} /qemu
 USER ${USER}
 ENV HOME ${HOME}
 EOF
