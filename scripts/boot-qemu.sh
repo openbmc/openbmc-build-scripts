fd3301eb37c5e71c3cf5b8023709741fe40a3828
Alanny Lopez <alanny.lopez@ibm.com>
2017-07-27 10:25:50 -0500

    Fix for the qemu not launching properly
    QEMU was not launching before, this fix should resolve that and allow for the
    QEMU to boot to login.
    
    Change-Id: I5f419d29c8135ff70110c0295a5a8734891c14af
    Signed-off-by: Alanny Lopez <alanny.lopez@ibm.com>

--- scripts/boot-qemu.sh
+++ scripts/boot-qemu.sh
@@ -1,12 +1,25 @@
 #!/bin/bash -xe
-#
+###############################################################################
 # Launch QEMU using the raw commands
+#
+#  Can be run by specifying the BASE_DIR and QEMU_ARCH when script is called.
+#  Additionally this script is automatically called by running the 
+#  run-robot-qemu-test.sh, it used to launch the container used in that script.
+#
+###############################################################################
+#
+#  Variables BASE_DIR and QEMU_ARCH are both required but can be optionally
+#  input as parameters following the initial script call. Alternatively, they
+#  can be input by exporting them or sourcing the script into an environment
+#  that has them declared.
 #
 #  Parameters:
 #   parm1:  <optional, QEMU architecture to use >
 #            default is ${QEMU_ARCH} - ppc64le-linux or x86_64-linux
 #   parm2:  <optional, full path to base directory of qemu binary and images >
 #            default is ${HOME}
+#
+###############################################################################
 
 set -uo pipefail
 
@@ -26,18 +39,21 @@ fi
 
 cd ${BASE_DIR}
 
+DRIVE=$(ls ./tmp/deploy/images/qemuarm | grep rootfs.ext4)
 ./tmp/sysroots/${QEMU_ARCH}/usr/bin/qemu-system-arm \
-    -nographic \
-    -kernel ./tmp/deploy/images/qemuarm/zImage-qemuarm.bin \
-    -machine versatilepb \
-    -drive file=./tmp/deploy/images/qemuarm/obmc-phosphor-image-qemuarm.ext4,format=raw \
-    -no-reboot \
-    -show-cursor \
-    -usb \
-    -usbdevice wacom-tablet \
-    -no-reboot -m 128 \
-    -redir tcp:22::22 \
-    -redir tcp:443::443 \
-    --append \
-    "root=/dev/sda rw console=ttyAMA0,115200 console=tty mem=128M highres=off \
-    rootfstype=ext4 console=ttyS0"
+     -redir tcp:443::443 \
+     -redir tcp:80::80 \
+     -redir tcp:22::22 \
+     -machine versatilepb \
+     -m 256 \
+     -drive file=./tmp/deploy/images/qemuarm/${DRIVE},if=virtio,format=raw \
+     -show-cursor \
+     -usb \
+     -usbdevice tablet \
+     -device virtio-rng-pci \
+     -serial mon:vc \
+     -serial mon:stdio \
+     -serial null \
+     -kernel ./tmp/deploy/images/qemuarm/zImage \
+     -append 'root=/dev/vda rw highres=off  console=ttyS0 mem=256M ip=dhcp console=ttyAMA0,115200 console=tty'\
+     -dtb ./tmp/deploy/images/qemuarm/zImage-versatile-pb.dtb
