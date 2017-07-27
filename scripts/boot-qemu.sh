784f389345d02b54ada07986dc1cf46622c07360
Alanny Lopez <alanny.lopez@ibm.com>
2017-07-27 10:25:50 -0500

    Updated QEMU run command and header
    QEMU was not launching before, the changes in this commit have allowed
    for the QEMU container to launch and allow user login. Also updated the
    header to be more descriptive.
    
    Change-Id: I5f419d29c8135ff70110c0295a5a8734891c14af
    Signed-off-by: Alanny Lopez <alanny.lopez@ibm.com>

--- scripts/boot-qemu.sh
+++ scripts/boot-qemu.sh
@@ -1,16 +1,30 @@
 #!/bin/bash -xe
-#
+###############################################################################
 # Launch QEMU using the raw commands
+#
+#  Can be run by specifying the BASE_DIR and QEMU_ARCH when script is called.
+#  Additionally this script is automatically called by running the
+#  run-robot-qemu-test.sh, it's used to launch the QEMU test container.
+#
+###############################################################################
+#
+#  Variables BASE_DIR and QEMU_ARCH are both required but can be optionally
+#  input as parameters following the initial script call. Alternatively, they
+#  can be input by exporting them or sourcing the script into an environment
+#  that has them declared.
 #
 #  Parameters:
 #   parm1:  <optional, QEMU architecture to use >
 #            default is ${QEMU_ARCH} - ppc64le-linux or x86_64-linux
 #   parm2:  <optional, full path to base directory of qemu binary and images >
 #            default is ${HOME}
+#
+###############################################################################
 
 set -uo pipefail
 
 QEMU_ARCH=${1:-$QEMU_ARCH}
+# If QEMU_ARCH is empty exit, it is required to continue
 echo "QEMU_ARCH = $QEMU_ARCH"
 if [[ -z $QEMU_ARCH ]]; then
     echo "Did not pass in required QEMU arch parameter"
@@ -18,26 +32,34 @@ if [[ -z $QEMU_ARCH ]]; then
 fi
 
 BASE_DIR=${2:-$HOME}
+# If BASE_DIR doesn't exist exit, it is required to continue
 echo "BASE_DIR = $BASE_DIR"
 if [[ ! -d $BASE_DIR ]]; then
     echo "No input directory and HOME not set!"
     exit -1
 fi
 
+# Enter the base directory
 cd ${BASE_DIR}
 
+# Find the correct drive file, and save its name
+DRIVE=$(ls ./tmp/deploy/images/qemuarm | grep rootfs.ext4)
+
+# Launch QEMU using the qemu-system-arm
 ./tmp/sysroots/${QEMU_ARCH}/usr/bin/qemu-system-arm \
-    -nographic \
-    -kernel ./tmp/deploy/images/qemuarm/zImage-qemuarm.bin \
+    -redir tcp:443::443 \
+    -redir tcp:80::80 \
+    -redir tcp:22::22 \
     -machine versatilepb \
-    -drive file=./tmp/deploy/images/qemuarm/obmc-phosphor-image-qemuarm.ext4,format=raw \
-    -no-reboot \
+    -m 256 \
+    -drive file=./tmp/deploy/images/qemuarm/${DRIVE},if=virtio,format=raw \
     -show-cursor \
     -usb \
-    -usbdevice wacom-tablet \
-    -no-reboot -m 128 \
-    -redir tcp:22::22 \
-    -redir tcp:443::443 \
-    --append \
-    "root=/dev/sda rw console=ttyAMA0,115200 console=tty mem=128M highres=off \
-    rootfstype=ext4 console=ttyS0"
+    -usbdevice tablet \
+    -device virtio-rng-pci \
+    -serial mon:vc \
+    -serial mon:stdio \
+    -serial null \
+    -kernel ./tmp/deploy/images/qemuarm/zImage \
+    -append 'root=/dev/vda rw highres=off  console=ttyS0 mem=256M ip=dhcp console=ttyAMA0,115200 console=tty'\
+    -dtb ./tmp/deploy/images/qemuarm/zImage-versatile-pb.dtb
