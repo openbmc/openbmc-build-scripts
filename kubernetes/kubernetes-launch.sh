b3d0091f9120440673b9483211b95800283734c9
Alanny Lopez <alanny.lopez@ibm.com>
2017-08-01 16:14:41 -0500

    Launch script will now tag instead of build images
    Made this change to shift the building of the Dockerfile away from the
    helper script. Instead the build will now be done by invoker scripts.
    Then doing the helper script will tag that built image with a new name
    that will be used to replace the old image name. This change updates the
    build-setup.sh, the qemu-build.sh, and the kubernetes-launch.sh
    
    Change-Id: I2d82e30b5e427bc46ac327541c58eebe9e6efd52
    Signed-off-by: Alanny Lopez <alanny.lopez@ibm.com>

--- kubernetes/kubernetes-launch.sh
+++ kubernetes/kubernetes-launch.sh
@@ -59,14 +59,14 @@ case ${invoker} in
     hclaim=${hclaim:-jenkins}
     sclaim=${sclaim:-shared-state-cache}
     oclaim=${oclaim:-openbmc-reference-repo}
-    imgname=${imgname:-${imgrepo}${distro}:${imgtag}-${ARCH}}
+    newimgname=${newimgname:-${imgrepo}${distro}:${imgtag}-${ARCH}}
     podname=${podname:-openbmc${BUILD_ID}-${target}-builder}
     ;;
   QEMU-build)
     podname=${podname:-qemubuild${BUILD_ID}}
     hclaim=${hclaim:-jenkins}
     qclaim=${qclaim:-qemu-repo}
-    imgname="${imgrepo}${imgname}"
+    newimgname="${imgrepo}${imgname}"
     ;;
   QEMU-launch)
     ;;
@@ -80,8 +80,9 @@ case ${invoker} in
 esac
 
 
-# Build the Docker image, using the Dockerfile carried from build-setup.sh
-docker build -t ${imgname} - <<< "${Dockerfile}"
+# Tag the image created by the invoker with the new image name that includes the imgrepo
+docker tag ${imgname} ${newimgname}
+imgname=${newimgname}
 
 # Push the image that was built to the image repository
 docker push ${imgname}
@@ -92,7 +93,8 @@ kubectl create -f - <<< "${yamlfile}"
 # Once pod is running track logs
 if [[ "${log}" == true ]]; then
   # Wait for Pod to be running
-  while [ -z "$(kubectl describe pod ${podname} -n ${namespace} | grep Status: | grep Running)" ]; do
+  while [ -z "$(kubectl describe pod ${podname} -n ${namespace} | grep Status: | grep Running)" ]
+  do
     if [ ${timeout} -lt 0 ];then
       kubectl delete -f - <<< "${yamlfile}"
       echo "Timeout Occured: Job failed to start running in time"
