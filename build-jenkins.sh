f6989d90c84debc221dfff1ade2193a47f57f86b
Alanny Lopez <Alanny.Lopez@ibm.com>
2017-10-18 11:43:20 -0500

    Add kubernetes integration into build-jenkins.sh
    Added functionality so that the build-jenkins.sh can be used to launch
    the jenkins master into a kubernetes cluster. To go alongside this added
    a helper script which can be used to launch all the kubernetes components
    such as PVC's and Docker Config secrets needed to do the openbmc builds
    on Kubernetes.
    
    Change-Id: I0d03572cc1b5cb7b9b81b2e2cec88d5c02b34e16
    Signed-off-by: Alanny Lopez <Alanny.Lopez@ibm.com>

--- build-jenkins.sh
+++ build-jenkins.sh
@@ -45,7 +45,7 @@
 #                     Default: "8080"
 #  agent_port         The port used as the Jenkins slave agent port
 #                     Default: "50000"
-#  out_img            The name given to the Docker image when it is built
+#  imgname            The name given to the Docker image when it is built
 #                     Default: "openbmc/jenkins-master-${ARCH}:${JENKINS_VRSN}"
 #
 ################################################################################
@@ -72,7 +72,7 @@ j_gid=${j_gid:-1000}
 j_home=${j_home:-/var/jenkins_home}
 http_port=${http_port:-8080}
 agent_port=${agent_port:-50000}
-out_img=${out_img:-openbmc/jenkins-master-${ARCH}:${j_vrsn}}
+imgname=${imgname:-openbmc/jenkins-master-${ARCH}:${j_vrsn}}
 
 # Save the Jenkins.war URL to a variable and SHA if we care about verification
 j_url=https://repo.jenkins-ci.org/public/org/jenkins-ci/main/jenkins-war/${j_vrsn}/jenkins-war-${j_vrsn}.war
@@ -187,7 +187,7 @@ EOF
 ################################################################################
 
 # Build the image
-docker build -t ${out_img} .
+docker build -t ${imgname} .
 
 if [[ ${launch} == "docker" ]]; then
 
@@ -241,7 +241,7 @@ if [[ ${launch} == "docker" ]]; then
     -p ${http_port}:8080 \
     -p ${agent_port}:${agent_port} \
     --env JENKINS_OPTS=\"${options}\" \
-    ${out_img}
+    ${imgname}
 
 elif [[ ${launch} == "k8s" ]]; then
   # launch using the k8s template
