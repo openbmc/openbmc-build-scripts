24cf9f1a2f68190cc1285c3dadeb403c16cea33e
Adriana Kobylak <anoo@us.ibm.com>
2018-01-10 16:58:27 -0600

    format-code.sh: Add pycodestyle (pep8) check
    Check pycodestyle (former pep8) and exit if there are any
    errors.
    
    Also move the checking of the formatting files to the
    format-code.sh file.
    
    Change-Id: Idca20d145e36a471634666aa39c968d3542886d8
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- scripts/format-code.sh
+++ scripts/format-code.sh
@@ -13,6 +13,16 @@ DIR=$1
 cd ${DIR}
 
 echo "Formatting code under $DIR/"
-find . -regextype sed -regex ".*\.[hc]\(pp\)\?" -not -name "*mako*" -print0 | xargs -0 "clang-format-3.9" -i
 
-git --no-pager diff --exit-code
+if [ -f "setup.cfg" ]; then
+  pycodestyle --show-source .
+  rc=$?
+  if [ ${rc} -ne 0 ]; then
+    exit ${rc}
+  fi
+fi
+
+if [ -f ".clang-format" ]; then
+  find . -regextype sed -regex ".*\.[hc]\(pp\)\?" -not -name "*mako*" -print0 | xargs -0 "clang-format-3.9" -i
+  git --no-pager diff --exit-code
+fi
