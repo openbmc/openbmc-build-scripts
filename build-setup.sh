f664ac4f1dde33e06d1f2cb00f9ce734bdcdb645
Joel Stanley <joel@jms.id.au>
2018-08-28 11:51:04 -0700

    build-setup: Clean up removed machines
    Barreleye, Firestone and Garrison are no longer part of the OpenBMC
    tree.
    
    Change-Id: I35e27fef54e8f3230552a31ebf15f5cd016d733d
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- build-setup.sh
+++ build-setup.sh
@@ -112,20 +112,11 @@ chown ${UID}:${GROUPS} ${xtrct_path}
 
 # Work out what build target we should be running and set BitBake command
 case ${target} in
-  barreleye)
-    BITBAKE_CMD="TEMPLATECONF=meta-openbmc-machines/meta-openpower/meta-rackspace/meta-barreleye/conf source oe-init-build-env"
-    ;;
   palmetto)
     BITBAKE_CMD="TEMPLATECONF=meta-openbmc-machines/meta-openpower/meta-ibm/meta-palmetto/conf source oe-init-build-env"
     ;;
   witherspoon)
     BITBAKE_CMD="TEMPLATECONF=meta-openbmc-machines/meta-openpower/meta-ibm/meta-witherspoon/conf source oe-init-build-env"
-    ;;
-  firestone)
-    BITBAKE_CMD="TEMPLATECONF=meta-openbmc-machines/meta-openpower/meta-ibm/meta-firestone/conf source oe-init-build-env"
-    ;;
-  garrison)
-    BITBAKE_CMD="TEMPLATECONF=meta-openbmc-machines/meta-openpower/meta-ibm/meta-garrison/conf source oe-init-build-env"
     ;;
   evb-ast2500)
     BITBAKE_CMD="TEMPLATECONF=meta-openbmc-machines/meta-evb/meta-evb-aspeed/meta-evb-ast2500/conf source oe-init-build-env"
