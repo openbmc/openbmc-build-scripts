a7b4b852794877f148fd6d2bcc33a77d7c1c01f1
Andrew Geissler <geissonator@yahoo.com>
2019-01-23 10:44:21 -0600

    Default to using robot directly
    The docker containers are configured to have all dependencies
    installed so no need for tox. Just use robot directly.
    
    Change-Id: Ie7105a1ed500fe246b01b520f3d66722e3b0739f
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

--- scripts/run-robot.sh
+++ scripts/run-robot.sh
@@ -20,7 +20,11 @@
 
 MACHINE=${MACHINE:-"qemu"}
 ROBOT_CODE_HOME=${ROBOT_CODE_HOME:-/tmp/$(whoami)/${RANDOM}/obmc-robot/}
-ROBOT_TEST_CMD=${ROBOT_TEST_CMD:-"tox -e ${MACHINE} -- --argumentfile test_lists/QEMU_CI tests"}
+ROBOT_TEST_CMD=${ROBOT_TEST_CMD:-"python -m robot\
+    -v OPENBMC_HOST:${IP_ADDR}\
+    -v SSH_PORT:${SSH_PORT}\
+    -v HTTPS_PORT:${HTTPS_PORT}\
+    --argumentfile ./test_lists/QEMU_CI ./tests"}
 
 git clone https://github.com/openbmc/openbmc-test-automation.git \
         ${ROBOT_CODE_HOME}
