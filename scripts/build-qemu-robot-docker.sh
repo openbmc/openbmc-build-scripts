ee51f047a6f7989057a32b86f5777886c9577511
George Keishing <gkeishin@in.ibm.com>
2019-03-05 13:55:29 -0600

    DMTF Redfish-JsonSchema-ResponseValidator package install
    Changes:
       - Refer: https://github.com/DMTF/Redfish-JsonSchema-ResponseValidator
         pip3 install jsonschema
    
    Change-Id: If859c532a97f1e375a1a5e5a04ad11839bc692cb
    Signed-off-by: George Keishing <gkeishin@in.ibm.com>

--- scripts/build-qemu-robot-docker.sh
+++ scripts/build-qemu-robot-docker.sh
@@ -82,7 +82,8 @@ RUN pip install \
 
 RUN pip3 install \
     beautifulsoup4 --upgrade \
-    lxml
+    lxml \
+    jsonschema
 
 RUN wget https://sourceforge.net/projects/ipmitool/files/ipmitool/1.8.18/ipmitool-1.8.18.tar.bz2
 RUN tar xvfj ipmitool-*.tar.bz2
