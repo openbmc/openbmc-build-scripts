16a57ac27a29529ad775a7fb21dddb76495e1926
William A. Kennington III <wak@google.com>
2019-03-27 19:53:18 -0700

    build-unit-test-docker: Build gpioplus with meson
    The autotools build will eventually be deleted so we should use meson
    instead since that is now the primary build system.
    
    Change-Id: I75281d4c5e287dba77f5aa3037569d9c5487ca3f
    Signed-off-by: William A. Kennington III <wak@google.com>

--- build-unit-test-docker.sh
+++ build-unit-test-docker.sh
@@ -320,10 +320,9 @@ ninja -C build install
 FROM openbmc-base as openbmc-gpioplus
 RUN curl -L https://github.com/openbmc/gpioplus/archive/${PKG_REV['gpioplus']}.tar.gz | tar -xz && \
 cd gpioplus-* && \
-./bootstrap.sh && \
-./configure ${CONFIGURE_FLAGS[@]} --disable-tests --disable-examples && \
-make -j$(nproc) && \
-make install
+meson build -Dprefix=${PREFIX} -Dtests=disabled -Dexamples=false && \
+ninja -C build && \
+ninja -C build install
 
 FROM openbmc-base as openbmc-phosphor-dbus-interfaces
 COPY --from=openbmc-sdbusplus ${PREFIX} ${PREFIX}
