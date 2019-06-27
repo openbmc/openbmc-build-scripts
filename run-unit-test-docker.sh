5a8aa48c0f4365a4622ddde9265711b501dfb921
William A. Kennington III <wak@google.com>
2019-06-27 16:21:18 -0700

    run-unit-test-docker: Fix out of sync distro
    build-unit-test-docker was switched to eoan but this was missed during
    that change 0232a25bf96e8017a90d6d86985678d7b5681bf6.
    
    Change-Id: I56fbc973ee9c2c5837bff7593e30c1daa928a4d7
    Signed-off-by: William A. Kennington III <wak@google.com>

--- run-unit-test-docker.sh
+++ run-unit-test-docker.sh
@@ -31,7 +31,7 @@ set -uo pipefail
 # Default variables
 BRANCH=${BRANCH:-"master"}
 DOCKER_IMG_NAME=${DOCKER_IMG_NAME:-"openbmc/ubuntu-unit-test-${BRANCH}"}
-DISTRO=${DISTRO:-ubuntu:disco}
+DISTRO=${DISTRO:-ubuntu:eoan}
 OBMC_BUILD_SCRIPTS="openbmc-build-scripts"
 UNIT_TEST_PY_DIR="scripts"
 UNIT_TEST_PY="unit-test.py"
