2eb45d3a771c68f7a527f47b5372f130cb1bfc62
Andrew Geissler <geissonator@yahoo.com>
2019-09-18 21:56:44 -0500

    witherspoon-tacoma machine support
    The tacoma image requires a different target due to the way it is loaded
    from flash currently.
    
    Tested:
    Verified for witherspoon-tacoma:
        "bitbake aspeed-image-norootfs obmc-phosphor-debug-tarball"
    Verified for witherspoon:
        "bitbake obmc-phosphor-image obmc-phosphor-debug-tarball"
    Verified aspeed-norootfs-witherspoon-tacoma.bin generated when target is
    witherspoon-tacoma
    
    Change-Id: Ia568f0f12da80be4532ec052a05a96e19b717f3c
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

--- build-setup.sh
+++ build-setup.sh
@@ -35,6 +35,7 @@
 #  target             The target we aim to build:
 #                     evb-ast2500|palmetto|qemu|qemux86-64
 #                     romulus|s2600wf|witherspoon|zaius|tiogapass|gsj|mihawk
+#                     witherspoon-tacoma|
 #                     Default: "qemu"
 #  no_tar             Set to true if you do not want the debug tar built
 #                     Default: "false"
@@ -89,6 +90,7 @@ xtrct_small_copy_dir=${xtrct_small_copy_dir:-deploy/images}
 xtrct_path="${obmc_dir}/build/tmp"
 xtrct_copy_timeout="300"
 
+bitbake_target="obmc-phosphor-image"
 PROXY=""
 
 # Determine the architecture
@@ -149,6 +151,12 @@ case ${target} in
     LAYER_DIR="meta-ibm/meta-witherspoon"
     MACHINE="witherspoon-128"
     DISTRO="openbmc-witherspoon"
+    ;;
+  witherspoon-tacoma)
+    LAYER_DIR="meta-ibm/meta-witherspoon"
+    MACHINE="witherspoon-tacoma"
+    DISTRO="openbmc-openpower"
+    bitbake_target="aspeed-image-norootfs"
     ;;
   evb-ast2500)
     LAYER_DIR="meta-evb/meta-evb-aspeed/meta-evb-ast2500"
@@ -296,9 +304,8 @@ export PROXY_PORT=${http_proxy/#http*:\/\/*:}
 mkdir -p ${WORKSPACE}
 
 # Determine command for bitbake image build
-bitbake_image="obmc-phosphor-image"
 if [ $no_tar = "false" ]; then
-    bitbake_image="${bitbake_image} obmc-phosphor-debug-tarball"
+    bitbake_target="${bitbake_target} obmc-phosphor-debug-tarball"
 fi
 
 cat > "${WORKSPACE}"/build.sh << EOF_SCRIPT
@@ -366,9 +373,9 @@ EOF_CONF
 
 # Kick off a build
 if [[ -n "${nice_priority}" ]]; then
-    nice -${nice_priority} bitbake ${BITBAKE_OPTS} ${bitbake_image}
+    nice -${nice_priority} bitbake ${BITBAKE_OPTS} ${bitbake_target}
 else
-    bitbake ${BITBAKE_OPTS} ${bitbake_image}
+    bitbake ${BITBAKE_OPTS} ${bitbake_target}
 fi
 
 # Copy internal build directory into xtrct_path directory
