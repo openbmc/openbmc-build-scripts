bffcb16655e53015897b8b8e869a5647d327d6c0
Andrew Jeffery <andrew@aj.id.au>
2020-03-13 10:42:08 +1030

    unit-test: Exercise all of the repository's supported build systems
    If a repository supports multiple build systems (e.g. libmctp with
    autotools and cmake), attempt to build and test the code with each build
    system in turn. This helps ensure the build systems don't bitrot.
    
    Change-Id: Iec46f858beca43f0f8598132f816933ea6c8e6d1
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- scripts/unit-test.py
+++ scripts/unit-test.py
@@ -870,13 +870,16 @@ class Package(object):
         system.build()
         system.install()
 
-    def test(self):
-        system = self.build_system()
+    def _test_one(self, system):
         system.configure(True)
         system.build()
         system.install()
         system.test()
         system.analyze()
+
+    def test(self):
+        for system in self.build_systems():
+            self._test_one(system)
 
 
 def find_file(filename, basedir):
