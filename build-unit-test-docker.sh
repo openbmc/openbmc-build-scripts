6b8ae51286e411dd4a7a68f8b40e5298447dad9b
SunnySrivastava1984 <sunnsr25@in.ibm.com>
2020-06-03 08:46:42 -0500

    Enable openpower interfaces in Docker unit test
    This commit modifies the docker unit test script to enable
    openpower dbus interfaces.
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>
    Change-Id: I9778319b2566bffbb7196e4fb72a17f98e401f1b

--- build-unit-test-docker.sh
+++ build-unit-test-docker.sh
@@ -417,7 +417,7 @@ COPY --from=openbmc-sdbusplus ${PREFIX} ${PREFIX}
 RUN curl -L https://github.com/openbmc/phosphor-dbus-interfaces/archive/${PKG_REV['openbmc/phosphor-dbus-interfaces']}.tar.gz | tar -xz && \
 cd phosphor-dbus-interfaces-* && \
 ./bootstrap.sh && \
-./configure ${CONFIGURE_FLAGS[@]} --enable-openpower-dbus-interfaces && \
+./configure ${CONFIGURE_FLAGS[@]} --enable-openpower-dbus-interfaces --enable-ibm-dbus-interfaces && \
 make -j$(nproc) && \
 make install
 
