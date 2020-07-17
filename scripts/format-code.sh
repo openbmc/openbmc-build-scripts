1ecc583d473264203eb04c0eb46a2b017eecc5dd
Patrick Williams <patrick@stwcx.xyz>
2020-07-17 15:10:15 -0500

    shellcheck: enable optional shell linting
    Add to the `format-code.sh` optional support for linting shell scripts
    using `shellcheck`.  Repositories can enable this by touching an empty
    file `.shellcheck` in the root of the repository.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: Ie25c27cffc96b7fa66291abef1b0f90a0f1862ed

--- scripts/format-code.sh
+++ scripts/format-code.sh
@@ -24,6 +24,14 @@ if [[ -f "setup.cfg" ]]; then
   fi
 fi
 
+if [[ -f ".shellcheck" ]]; then
+  shell_scripts="$(git ls-files | xargs -n1 file -0 | \
+                   grep -a "shell script" | cut -d '' -f 1)"
+  for script in $shell_scripts; do
+    shellcheck -x "$script"
+  done
+fi
+
 # Allow called scripts to know which clang format we are using
 export CLANG_FORMAT="clang-format-10"
 IGNORE_FILE=".clang-ignore"
