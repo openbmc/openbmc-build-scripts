906646a2c4ff7b3a8f403cef4703e72f6123b06e
joseiciano <joseiciano@protonmail.com>
2021-06-18 15:09:58 +0000

    Buildhistory srcurl/srcrev collection script.
    Generates a "repositories.txt" file of the built image's dependencies.
    File format is "<src_url> <src_rev>". Bitbake's internal tools,
    specifically bitbake -a, allows for easy viewing/modification
    of the source revision, not the url. Issue is noted since 2019 at
    https://lists.openembedded.org/g/bitbake-devel/message/10421.
    To run the unit tests for a custom machine using the scripts in
    the codebase, such as `get_unit_test_report.py`, the url's are
    required at minimum.
    
    Testing: Tested manually through through romulus and olympus machines.
    Included in get_unit_test_report and tested when running the script.
    
    Signed-off-by: Jose Iciano <joseiciano@protonmail.com>
    Change-Id: Ic2d6067ee88671797f5eaa00efbec394deddd12b

---
+++ scripts/srcurl-rev-generator.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Creates a txt file of all src_uri's and src_rev's of the dependencies 
+# for the current image build. Data is structured in the following format:
+# <src_uri> <src_rev>
+# 
+# Script Variables: 
+#   build_history - Path to the build's build_history's collection of repo packages
+#   output_file - Path to where to create the script's output file. 
+
+build_history="$1"
+output_file=""$2"/repositories.txt"
+
+if [[ ! -d "${build_history}" ]]; then
+    echo "Path to buildhistory not found"
+    exit 1
+fi
+
+if [[ -f "${output_file}" ]]; then
+    rm -rf "${output_file}"
+fi
+
+for repo_dir in $(ls "${build_history}");
+do
+    src_url_file="${build_history}/${repo_dir}/latest"
+    src_rev_file="${build_history}/${repo_dir}/latest_srcrev"
+    
+    if [[ ! -f "${src_url_file}" ]]; then
+        continue
+    fi
+
+    url_info="$( tail -n 1 "${src_url_file}" )"
+    url_arr=(${url_info})
+    file_found=false
+
+    for dependency in "${url_arr[2]}";
+    do
+        if [[ "${dependency}" =~ ^"git://github.com/openbmc".* ]]; then
+            src_url="${dependency}"
+            file_found=true
+        fi
+    done
+
+    if [[ "${file_found}" == true ]]; then
+        # Get revision if available
+        if [[ -f "${src_rev_file}" ]]; then
+            rev_info=$( tail -n 1 "${src_rev_file}" )
+            rev_arr=("${rev_info}")
+            src_rev="${rev_arr[2]:1:-1}"
+
+            echo "${src_url}" "${src_rev}" >> "${output_file}"
+        else
+            echo "${src_url}" >> "${output_file}"
+        fi
+    fi
+
+done
