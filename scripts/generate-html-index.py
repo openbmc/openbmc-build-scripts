a37070b43ce3d40578fc6eb0c51bf973554b2f35
Patrick Williams <patrick@stwcx.xyz>
2022-12-05 10:33:46 -0600

    python: fix flake8 warnings and format with black
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: Ic54f4a3ecf328b0d2c65af5cfd9eb2048d671ad5

--- scripts/generate-html-index.py
+++ scripts/generate-html-index.py
@@ -13,7 +13,7 @@ import sys
 
 def main(i_raw_args):
     l_args = parse_args(i_raw_args)
-    create_index_file(l_args.logs_dir_path, '/', l_args.exclude)
+    create_index_file(l_args.logs_dir_path, "/", l_args.exclude)
 
 
 def create_index_file(i_dir_path, i_pretty_dir_path, i_exclude_list):
@@ -29,52 +29,65 @@ def create_index_file(i_dir_path, i_pretty_dir_path, i_exclude_list):
     i_exclude_list      A Python list of files and directories to exclude from
     """
 
-    l_index_file_path = os.path.join(i_dir_path, 'index.html')
+    l_index_file_path = os.path.join(i_dir_path, "index.html")
     l_sub_dir_list = os.listdir(i_dir_path)
 
     # Created a sorted list of sub-directories in this directory
     l_dirs = sorted(
-        [d for d
-         in l_sub_dir_list
-         if os.path.isdir(os.path.join(i_dir_path, d))
-            and d not in i_exclude_list])
+        [
+            d
+            for d in l_sub_dir_list
+            if os.path.isdir(os.path.join(i_dir_path, d))
+            and d not in i_exclude_list
+        ]
+    )
 
     # Create a sorted list of files in this directory
     l_files = sorted(
-        [f for f
-         in l_sub_dir_list
-         if not os.path.isdir(os.path.join(i_dir_path, f))
-            and f not in i_exclude_list])
+        [
+            f
+            for f in l_sub_dir_list
+            if not os.path.isdir(os.path.join(i_dir_path, f))
+            and f not in i_exclude_list
+        ]
+    )
 
     # Open up the index file we're going to write to.
-    with open(l_index_file_path, 'w+') as l_index_file:
+    with open(l_index_file_path, "w+") as l_index_file:
         l_index_file.write(
-            '<html>\n'
-            '<head><title>' + i_pretty_dir_path + '</title></head>\n'
-            '<body>\n'
-            '<h2>OpenBMC Logs</h2>\n'
-            '<h3>' + i_pretty_dir_path + '</h3>\n')
+            "<html>\n<head><title>"
+            + i_pretty_dir_path
+            + "</title></head>\n<body>\n<h2>OpenBMC Logs</h2>\n<h3>"
+            + i_pretty_dir_path
+            + "</h3>\n"
+        )
 
         # Only show the link to go up a directory if this is not the root.
-        if not i_pretty_dir_path == '/':
-            l_index_file.write('<a href=".."><img src="/dir.png"> ..</a><br>\n')
+        if not i_pretty_dir_path == "/":
+            l_index_file.write(
+                '<a href=".."><img src="/dir.png"> ..</a><br>\n'
+            )
 
         # List directories first.
         for l_dir in l_dirs:
             l_index_file.write(
                 '<a href="%s"><img src="/dir.png"> %s</a><br>\n'
-                % (l_dir, l_dir))
+                % (l_dir, l_dir)
+            )
             create_index_file(
                 os.path.join(i_dir_path, l_dir),
-                i_pretty_dir_path + l_dir + '/',
-                i_exclude_list)
+                i_pretty_dir_path + l_dir + "/",
+                i_exclude_list,
+            )
 
         # List files second.
         for l_file in l_files:
-            l_index_file.write('<a href="%s"><img src="/file.png"> %s</a><br>\n'
-                               % (l_file, l_file))
+            l_index_file.write(
+                '<a href="%s"><img src="/file.png"> %s</a><br>\n'
+                % (l_file, l_file)
+            )
 
-        l_index_file.write('</body>\n</html>')
+        l_index_file.write("</body>\n</html>")
 
 
 def parse_args(i_raw_args):
@@ -88,21 +101,25 @@ def parse_args(i_raw_args):
     """
 
     parser = argparse.ArgumentParser(
-        description="%(prog)s will create index files that can be displayed "
-                    "as web pages in a given directory and all its "
-                    "sub-directories.",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description=(
+            "%(prog)s will create index files that can be displayed "
+            "as web pages in a given directory and all its "
+            "sub-directories."
+        ),
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
     parser.add_argument(
-        'logs_dir_path',
-        help='Directory containing the logs that should be uploaded.')
+        "logs_dir_path",
+        help="Directory containing the logs that should be uploaded.",
+    )
     parser.add_argument(
-        '--exclude',
-        nargs='+',
-        default=['.git', 'index.html'],
-        help='A space-delimited list of files to exclude from the index.'
+        "--exclude",
+        nargs="+",
+        default=[".git", "index.html"],
+        help="A space-delimited list of files to exclude from the index.",
     )
     return parser.parse_args(i_raw_args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(sys.argv[1:])
