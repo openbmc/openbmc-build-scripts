b0e4dc64e76d2cb92bdf80955f6eceb4e372b4c4
Bill Huey (Hui) <bill.huey@gmail.com>
2024-10-30 16:09:19 -0700

    amd64 platform hack
    Change-Id: Ibd24e15b761e5b130d4ccd18302613236bba1789
    Signed-off-by: Bill Huey (Hui) <bill.huey@gmail.com>

--- scripts/build-qemu-robot-docker.sh
+++ scripts/build-qemu-robot-docker.sh
@@ -106,7 +106,7 @@ RUN apt-get update -qqy \
   && mv /opt/firefox /opt/firefox-112.0.2 \
   && ln -fs /opt/firefox-112.0.2/firefox /usr/bin/firefox
 
-ENV HOME ${HOME}
+ENV HOME=${HOME}
 
 ${PIP_MIRROR_CMD}
 
@@ -160,4 +160,4 @@ fi
 
 # Build above image
 # shellcheck disable=SC2086 # PROXY_ARGS is intentionally word-split.
-docker build ${PROXY_ARGS} -t "${DOCKER_IMG_NAME}" - <<< "${Dockerfile}"
+docker buildx build --push --platform linux/amd64,linux/arm64 --tag bhueyhui/openbmc-qemu-docker:experimental - <<< "${Dockerfile}"
